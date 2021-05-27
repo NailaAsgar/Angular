4936037e154d4bed8a094e421db1ddac3b5427b6
Naila Asgar <nailaasgar2796@gmail.com>
2021-05-27 16:48:59 +0530

    creted menu component

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,21 @@
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured!: boolean;
+    label: string;
+    price: string;
+    description: string;
+
+    constructor(){
+        this.id="";
+        this.name="";
+        this.image="";
+        this.category="";
+        this.label="";
+        this.price="";
+        this.description="";
+    }
+
+}
