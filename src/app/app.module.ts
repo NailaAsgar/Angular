987fce1e678d2ef73bfac66e379c23a0910347e0
NailaAsgar <nailaasgar2796@gmail.com>
2021-05-31 12:03:15 +0530

    Menu component display changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list'
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -22,7 +23,8 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
